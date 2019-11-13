cb6a2d530ba80e177ffa391891b298b3c6ad596e
lomakin <lomakin@A24PC6>
2019-11-13 17:53:02 +0200

    fix merge

--- CRUDCore/ClientApp/src/components/Home.js
+++ CRUDCore/ClientApp/src/components/Home.js
@@ -110,25 +110,25 @@ class Home extends Component {
                 <div>item 8</div>
                 <p><a href="#top">Наверх</a></p>
             </div>
-            <div>
-                  <Pagination>
-                    <Pagination.First />
-                    <Pagination.Prev />
-                    <Pagination.Item>{1}</Pagination.Item>
-                    <Pagination.Ellipsis />
+            // <div>
+            //       <Pagination>
+            //         <Pagination.First />
+            //         <Pagination.Prev />
+            //         <Pagination.Item>{1}</Pagination.Item>
+            //         <Pagination.Ellipsis />
 
-                    <Pagination.Item>{10}</Pagination.Item>
-                    <Pagination.Item>{11}</Pagination.Item>
-                    <Pagination.Item active>{12}</Pagination.Item>
-                    <Pagination.Item>{13}</Pagination.Item>
-                    <Pagination.Item disabled>{14}</Pagination.Item>
+            //         <Pagination.Item>{10}</Pagination.Item>
+            //         <Pagination.Item>{11}</Pagination.Item>
+            //         <Pagination.Item active>{12}</Pagination.Item>
+            //         <Pagination.Item>{13}</Pagination.Item>
+            //         <Pagination.Item disabled>{14}</Pagination.Item>
 
-                    <Pagination.Ellipsis />
-                    <Pagination.Item>{20}</Pagination.Item>
-                    <Pagination.Next />
-                    <Pagination.Last />
-                </Pagination>
-            </div>
+            //         <Pagination.Ellipsis />
+            //         <Pagination.Item>{20}</Pagination.Item>
+            //         <Pagination.Next />
+            //         <Pagination.Last />
+            //     </Pagination>
+            // </div>
         );
     }
 }
