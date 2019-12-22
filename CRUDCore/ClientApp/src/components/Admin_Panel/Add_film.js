e3b5c714fb6e2a1b6e442b061eb8c2206b1da8df
lomakin <lomakin@A24PC4>
2019-12-22 14:55:44 +0200

    admin panel

--- CRUDCore/ClientApp/src/components/Admin_Panel/Add_film.js
+++ CRUDCore/ClientApp/src/components/Admin_Panel/Add_film.js
@@ -1,41 +1,53 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './Admin.css';
 class SignUpForm extends Component {
-    state = {
-        name: '',
-      }
-      handleChange = event => {
-        this.setState({ name: event.target.value });
-      }
-     
-      handleSubmit = event => {
-        event.preventDefault();
-     
-        const user= {
-          name: this.state.name
-          
-        };
-     
-        // axios.post(`api/Account/upload`, { user })
-        //   .then(res => {
-        //     console.log(res);
-        //   })
-      }
-      logconsole = event => {
-        //console.log(getElementId("s").value);
-      }
-      render() {
-        return (
-          <div>
-            <form onSubmit={this.handleSubmit}>
-              <label>
-               film: 
-                <input id="s" type="text" name="name" onChange={this.handleChange} />
-              </label>
-              <button onClick={this.logconsole} type="submit">add film</button>
-            </form>
-          </div>
-        )
-      }
+
+
+  logconsole() {
+    console.log(document.getElementById('d1').value);
+    console.log(document.getElementById('d2').value);
+    console.log(document.getElementById('d3').value);
+    console.log(document.getElementById('d4').value);
+  }
+  render() {
+    return (
+      <div className="centeradm">
+
+        <div>
+        
+          <input type="text"
+            className="admintext"
+            id="d1"
+            name="Name"
+          />
+        </div>
+        <div>
+          <input type="number"
+            className="admintext"
+            id="d2"
+            name="Year"
+          />
+        </div>
+        <div>
+          <input type="text"
+            className="admintext"
+            id="d3"
+            name="country"
+          />
+        </div>
+        <div>
+          <input type="text"
+            className="admintext"
+            id="d3"
+            name="director"
+          />
+        </div>
+
+        <button id="d1" id="d2" id="d3" d="d4" onClick={this.logconsole} type="submit">add film</button>
+
+      </div>
+    )
+  }
 }
 export default (SignUpForm);
